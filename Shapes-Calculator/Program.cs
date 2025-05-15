7d067bc72efa76ee2b7f7e8054777b80f4e14ead
Kaushik Ram <22810981+Kaushik-A-Ram@users.noreply.github.com>
2025-05-15 14:49:47 +1000

    Add project files.

---
+++ Shapes-Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Shapes_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            float result;
+
+            Console.WriteLine("which shape do you want to find the area of?");
+
+            answer = Console.ReadLine().ToLower();
+
+            if (answer == "rectangle")
+            {
+                Console.WriteLine("enter the height of rectangle");
+                // convert user input into float
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("enter the width of rectangle");
+                // convert user input into float
+                float width = float.Parse(Console.ReadLine());
+
+                result = height * width;
+            }
+        }
+    }
+}
