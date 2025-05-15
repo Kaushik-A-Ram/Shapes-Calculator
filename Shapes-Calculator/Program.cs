1b9d751d65f68954dff8c47e70a2536e783da1f4
Kaushik Ram <22810981+Kaushik-A-Ram@users.noreply.github.com>
2025-05-15 20:23:11 +1000

    Finished

--- Shapes-Calculator/Program.cs
+++ Shapes-Calculator/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             string answer;
-            float result;
+            float result = 0;
 
-            Console.WriteLine("which shape do you want to find the area of?");
+            Console.WriteLine("which shape do you want to find the area of? (square, rectangle, circle, trapezoid)");
 
             answer = Console.ReadLine().ToLower();
 
@@ -23,6 +23,40 @@
 
                 result = height * width;
             }
+            else if (answer == "circle")
+            {
+                Console.WriteLine("enter radius of the circle");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius * radius);
+            }
+            else if (answer == "square")
+            {
+                Console.WriteLine("enter the height of square");
+                float height = float.Parse((Console.ReadLine()));
+
+                Console.WriteLine("enter the width of square");
+                float width = float.Parse((Console.ReadLine()));
+
+                result = height * width;
+            }
+            else if (answer == "trapezoid")
+            {
+                Console.WriteLine("enter base 1 of trapezoid");
+                float base1 = float.Parse((Console.ReadLine()));
+
+                Console.WriteLine("enter base 2 of trapezoid");
+                float base2 = float.Parse((Console.ReadLine()));
+
+                Console.WriteLine("enter the height of trapezoid");
+                float height = float.Parse((Console.ReadLine()));
+
+                result = ((base1 + base2) / 2) * height;
+            } else
+            {
+                Console.WriteLine("enter a valid shape (square, rectangle, circle, trapezoid)");
+            }
+                Console.WriteLine("the result is " + result);
         }
     }
 }
